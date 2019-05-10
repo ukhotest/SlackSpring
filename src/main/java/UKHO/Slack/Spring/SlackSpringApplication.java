3ad0f6938260d40ada93dee94f6afcd7882aca51
Marc Denman <MarcDenman@users.noreply.github.com>
2019-05-10 14:14:47 +0100

    Initial commit from Atomist

---
+++ src/main/java/UKHO/Slack/Spring/SlackSpringApplication.java
@@ -0,0 +1,12 @@
+package UKHO.Slack.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/UKHO/Slack/Spring/SlackSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package UKHO.Slack.Spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
